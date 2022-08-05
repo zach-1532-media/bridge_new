66bd28e093d817bab267b40c8f25b88349acb88c
zach-1532-media <93880226+zach-1532-media@users.noreply.github.com>
2022-08-05 15:07:07 -0400

    applicant grid and uploads

--- components/shared/backdrop/index.jsx
+++ components/shared/backdrop/index.jsx
@@ -7,7 +7,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 const BackDrop = ({ openBackdrop }) => (
   <Backdrop
-    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    sx={{ color: '#fefefefe', zIndex: (theme) => theme.zIndex.drawer + 1 }}
     open={openBackdrop}
   >
     <CircularProgress color="inherit" />
