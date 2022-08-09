3572547943178abb5be959cafbe8fd825df1a106
zach-1532-media <93880226+zach-1532-media@users.noreply.github.com>
2022-08-09 17:26:54 -0400

    updated confirmation page

---
+++ components/confirmation/index.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import CheckIcon from '@mui/icons-material/Check';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+
+import Container from '../front_components/container';
+
+const Confirmation = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const items = [
+    {
+      name: 'facebook',
+      href: 'https://www.facebook.com/ConnectAtTheBridge',
+      icon: <FacebookIcon sx={{ color: ' #4267B2' }} />,
+    },
+    {
+      name: 'twitter',
+      href: 'https://twitter.com/_The_Bridge11',
+      icon: <TwitterIcon sx={{ color: '#1DA1F2' }} />,
+    },
+    {
+      name: 'instagram',
+      href: 'https://www.instagram.com/connectatthebridge/',
+      icon: <InstagramIcon sx={{ color: '#fb3958' }} />,
+    },
+  ];
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <CheckIcon sx={{ fontSize: '5em', color: '#00FF00', mb: '.5em' }} />
+        <Typography
+          sx={{
+            fontSize: '6.5em',
+            fontWeight: '600',
+            mb: '.5em',
+          }}
+        >
+          Thank You!
+        </Typography>
+        <Box
+          sx={{
+            width: '50%',
+            alignItems: 'center',
+          }}
+        >
+          <Typography
+            variant="h4"
+            align="center"
+            sx={{
+              color: 'gray',
+              mb: '4em',
+            }}
+          >
+            Your job has been successfully posted! You'll recieve a confirmation
+            email shortly. Check it out by clicking the button below!
+          </Typography>
+        </Box>
+        <Link href={`/dashboards/business/postedJobs/${id}`} passHref>
+          <Button
+            size="large"
+            variant="contained"
+            sx={{ width: '40%', mb: '4em' }}
+          >
+            Check out your posted jobs
+          </Button>
+        </Link>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignContent: 'center',
+          }}
+        >
+          <Stack direction="row" spacing={2}>
+            {items.map((item) => (
+              <IconButton href={item.href} target="_blank" key={`${item.name}`}>
+                {item.icon}
+              </IconButton>
+            ))}
+          </Stack>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Confirmation;
--- pages/dashboards/business/confirmation/[id]/index.js
+++ pages/dashboards/business/confirmation/[id]/index.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+/* eslint-disable global-require */
 import { React, useEffect } from 'react';
 
 import { useRouter } from 'next/router';
@@ -8,17 +11,18 @@ import PropTypes from 'prop-types';
 import dbConnect from '../../../../../lib/dbConnect';
 import Business from '../../../../../models/Business';
 
+import Confirmation from '../../../../../components/confirmation';
 import UserBoxLinks from '../../../../../components/shared/layoutLinks/links';
 import MenuItems from '../../../../../components/shared/layoutLinks/items';
 
 import Dash from '../../../../../layouts/dash';
 
-const JobConfirmation = ({ data }) => {
+const JobConfirmation = ({ data, paymentInfo }) => {
   const router = useRouter();
   const { id } = router.query;
 
   useEffect(() => {
-    const item = localStorage.getItem('form');
+    const item = localStorage.getItem('jobForm');
     const form = JSON.parse(item);
     const paymentID = router.query.payment_intent;
     const { id } = router.query;
@@ -64,19 +68,22 @@ const JobConfirmation = ({ data }) => {
         />
       }
     >
-      <h1>Confirmation Page</h1>
+      <Confirmation />
     </Dash>
   );
 };
 
-export async function getServerSideProps({ query: { id } }) {
+export async function getServerSideProps({ query: { id, payment_intent } }) {
+  const stripe = require('stripe')(process.env.STRIPE_SECRET);
   await dbConnect();
 
+  const paymentIntent = await stripe.paymentIntents.retrieve(payment_intent);
   const business = await Business.findById(id);
 
   return {
     props: {
       data: JSON.parse(JSON.stringify(business)),
+      paymentInfo: JSON.parse(JSON.stringify(paymentIntent)),
     },
   };
 }
@@ -84,6 +91,7 @@ export async function getServerSideProps({ query: { id } }) {
 JobConfirmation.propTypes = {
   /* eslint-disable react/forbid-prop-types */
   data: PropTypes.object.isRequired,
+  paymentInfo: PropTypes.object.isRequired,
 };
 
 export default JobConfirmation;
